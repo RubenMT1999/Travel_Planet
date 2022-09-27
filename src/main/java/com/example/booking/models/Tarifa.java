7365ce57adfb57f4f5464a11e94af737dda5f258
Pedrofdez97 <pfernandezsalas@safareyes.es>
2022-09-27 19:28:10 +0200

    Clases de tarifa, temporada y pension

---
+++ src/main/java/com/example/booking/models/Tarifa.java
@@ -0,0 +1,36 @@
+package com.example.booking.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tarifa")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Tarifa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_temporada")
+    private Temporada temporada;
+
+    @ManyToOne
+    @JoinColumn(name = "id_pension")
+    private Pension pension;
+
+    @Column(name = "precio_persona")
+    private boolean precioPersona;
+
+
+
+
+}
+
