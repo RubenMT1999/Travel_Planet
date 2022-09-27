5f7f5961fbfddad505f2a1cc4a405d1976a1c78b
Pedrofdez97 <pfernandezsalas@safareyes.es>
2022-09-27 20:30:44 +0200

    enum de pension y temporada

--- src/main/java/com/example/booking/models/Pension.java
+++ src/main/java/com/example/booking/models/Pension.java
@@ -1,28 +1,5 @@
 package com.example.booking.models;
 
-import lombok.*;
-
-import javax.persistence.*;
-import javax.validation.constraints.NotEmpty;
-@Entity
-@Table(name = "pension")
-@Getter @Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class Pension {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-
-    @Column(name = "completa")
-    private boolean completa;
-
-    @Column(name = "desayuno_cena")
-    private boolean desayuno_Cena;
-
-    @Column(name = "desayuno")
-    private boolean desayuno;
-
-    @Column(name = "ninguna")
-    private boolean ninguna;
+public enum Pension {
+    Completa, Desayuno_Cena, Desayuno, Ninguna
 }
