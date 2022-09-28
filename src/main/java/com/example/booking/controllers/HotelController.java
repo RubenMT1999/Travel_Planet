42abcbd013fde6735bd6d39ea14ff8afdc1b8ba6
Pedrofdez97 <pfernandezsalas@safareyes.es>
2022-09-28 20:59:25 +0200

    enum de pension y temporada

---
+++ src/main/java/com/example/booking/controllers/HotelController.java
@@ -0,0 +1,13 @@
+package com.example.booking.controllers;
+
+import com.example.booking.models.Hotel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+    @Controller
+    public class HotelController {
+
+}
--- src/main/java/com/example/booking/repository/HotelRepository.java
+++ src/main/java/com/example/booking/repository/HotelRepository.java
@@ -2,8 +2,18 @@ package com.example.booking.repository;
 
 import com.example.booking.models.Hotel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface HotelRepository extends JpaRepository<Hotel,Integer> {
+    @Query("SELECT hh.ciudad, r.fecha_inicio, r.fecha_fin" +
+            "FROM hotel hh" +
+            "JOIN habitacion h  ON hh.id = h.id_hotel" +
+            "join reserva r on r.id_habitacion = h.id" +
+            "where hh.ciudad like %?1% and r.fecha_inicio like %?1% and r.fecha_fin like %?1%")
+    public List<Hotel> search(String keyword);
+
 }
