6081ab1b55c7a68fb21b52f37d3a37b00c0df8e1
rubenmt99 <matiastiscar@gmail.com>
2022-10-08 18:10:30 +0200

    reservas html

--- src/main/java/com/example/booking/controllers/ReservaController.java
+++ src/main/java/com/example/booking/controllers/ReservaController.java
@@ -4,6 +4,8 @@ package com.example.booking.controllers;
 import com.example.booking.models.Reserva;
 import com.example.booking.services.ReservaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,8 +23,9 @@ public class ReservaController {
     private ReservaService reservaService;
 
     @GetMapping("/ver")
-    public String listarReserva(Model model){
-        List<Reserva> reservas = reservaService.listar();
+    public String listarReserva(Model model, Authentication auth){
+        auth = SecurityContextHolder.getContext().getAuthentication();
+        List<Reserva> reservas = reservaService.reservasPorNombre(auth.getName());
         model.addAttribute("titulo", "Mis Reservas");
         model.addAttribute("reservas",reservas);
         return "reservas";
