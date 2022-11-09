cbfe2d9d4337b2b1374a3776d5a7c74453b66a88
RubenMT1999 <rmatiastiscar@safareyes.es>
2022-11-09 20:18:22 +0100

    graphyql

---
+++ src/main/java/com/example/booking/controllers/GrapQLController.java
@@ -0,0 +1,14 @@
+package com.example.booking.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class GrapQLController {
+
+
+    @PostMapping("")
+
+}
