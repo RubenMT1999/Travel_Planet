56494bb8f77f8593cd83b7caf9c52556ac7ccf78
Pedro <pfernandezsalas@safareyes.es>
2022-12-05 17:09:38 +0100

    Reserva

--- src/main/java/com/example/booking/controllers/PerfilUsuarioController.java
+++ src/main/java/com/example/booking/controllers/PerfilUsuarioController.java
@@ -248,9 +248,9 @@ public class PerfilUsuarioController {
 
         model.addAttribute("metodoPago", pago);
 
-        if (reserva.getPagado() == true) {
-            redirectAttributes.addFlashAttribute("pagado", "La Reserva se ha pagado con éxito");
-        }
+
+        redirectAttributes.addFlashAttribute("success", "La Reserva se ha pagado con éxito");
+
 
         return "redirect:/perfil/mis-reservas";
     }
