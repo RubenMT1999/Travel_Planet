c316640b2c7de5a152451799ec4c77d408fbbc56
RubenMT1999 <rmatiastiscar@safareyes.es>
2022-12-06 14:13:05 +0100

    habitacionController documentado

--- src/main/java/com/example/booking/controllers/HabitacionController.java
+++ src/main/java/com/example/booking/controllers/HabitacionController.java
@@ -53,31 +53,66 @@ public class HabitacionController {
     @Autowired
     HotelService hotelService;
 
+    /**
+     * Este método maneja una solicitud GET a la ruta /habitaciones/listar/{id}.
+     * Acepta un parámetro de consulta llamado "page" que se utiliza para paginar los resultados.
+     *
+     * @param model El objeto Spring Model utilizado para almacenar atributos del modelo
+     * @param id El id de la entidad para la cual se listan las habitaciones
+     * @param page El número de página a recuperar, el valor predeterminado es 0
+     * @return El nombre de la plantilla Thymeleaf a renderizar
+     */
     @GetMapping({"/listar/{id}"})
-    public String listar(Model model, @PathVariable Integer id ,@RequestParam(name="page", defaultValue = "0") int page){
+    public String listar(Model model, @PathVariable Integer id, @RequestParam(name="page", defaultValue = "0") int page) {
         Pageable pageRequest = PageRequest.of(page, 5);
-        Page<Habitacion> habitaciones = habitacionService.listarHabitacionesPages(pageRequest,id);
-        PageRender<Habitacion> pageRender = new PageRender<>("/habitaciones/listar/"+id,habitaciones);
-        model.addAttribute("page",pageRender);
-        model.addAttribute("habitaciones",habitaciones);
-        model.addAttribute("id",id);
+        Page<Habitacion> habitaciones = habitacionService.listarHabitacionesPages(pageRequest, id);
+        PageRender<Habitacion> pageRender = new PageRender<>("/habitaciones/listar/"+id, habitaciones);
+        model.addAttribute("page", pageRender);
+        model.addAttribute("habitaciones", habitaciones);
+        model.addAttribute("id", id);
         return "habitaciones";
     }
 
+    /**
+     * Este método maneja una solicitud GET a la ruta /habitaciones/crear/{id}.
+     *
+     * @param model El objeto Spring Model utilizado para almacenar atributos del modelo
+     * @param id El id del Hotel al que se asociará la nueva Habitacion
+     * @return El nombre de la plantilla Thymeleaf a renderizar
+     */
     @GetMapping("/crear/{id}")
-    public String crear(Model model, @PathVariable Integer id){
+    public String crear(Model model, @PathVariable Integer id) {
         Habitacion habitacion = new Habitacion();
         Hotel hotel = hotelRepository.findById(id).get();
         habitacion.setHotel(hotel);
         Tarifa tarifa = hotel.getTarifa();
 
-        model.addAttribute("titulo","Crear Habitación");
-        model.addAttribute("habitacion",habitacion);
-        model.addAttribute("tarifa",tarifa);
+        model.addAttribute("titulo", "Crear Habitación");
+        model.addAttribute("habitacion", habitacion);
+        model.addAttribute("tarifa", tarifa);
         return "crearHabitacion";
     }
 
 
+    /**
+     * Este método maneja una solicitud POST a la ruta /habitaciones/crear.
+     * Valida el objeto Habitacion proporcionado y comprueba si existe una Habitacion con el mismo
+     * número en el mismo Hotel. Si hay errores de validación o si existe una Habitacion con el
+     * mismo número en el mismo Hotel, se renderiza la plantilla "crearHabitacion" de nuevo.
+     * Si la validación es exitosa, se establece el precio de la Habitacion en función de la Tarifa
+     * del Hotel y de los atributos de la Habitacion. También se carga la imagen proporcionada y se
+     * guarda la Habitacion en la base de datos. Finalmente, se redirige a la ruta
+     * /habitaciones/listar/{idHotel}.
+     *
+     * @param habitacion El objeto Habitacion a validar y guardar
+     * @param result El objeto BindingResult que contiene los resultados de la validación
+     * @param model El objeto Spring Model utilizado para almacenar atributos del modelo
+     * @param imagen La imagen a cargar para la Habitacion
+     * @param status El objeto SessionStatus que se utiliza para marcar la sesión como completada
+     * @param idHotel El id del Hotel al que pertenece la Habitacion
+     * @param flash El objeto RedirectAttributes utilizado para agregar atributos flash al modelo
+     * @return El nombre de la plantilla Thymeleaf a renderizar o la ruta a la que se redirige
+     */
     @PostMapping("/crear")
     public String procesar(@Valid Habitacion habitacion, BindingResult result, Model model, @RequestParam(value = "file") MultipartFile imagen,
                            SessionStatus status, @ModelAttribute("id") Integer idHotel, RedirectAttributes flash){
@@ -92,8 +127,6 @@ public class HabitacionController {
             return "crearHabitacion";
         }
 
-
-
         habitacion.setPrecioBase(habitacionService.establecerPrecioHabitacion(habitacion.getPrecioBase(), habitacion));
         habitacionService.editarDisponibilidad(true,habitacion.getId());
         habitacionService.cargarImagen(imagen,flash,habitacion);
@@ -107,7 +140,18 @@ public class HabitacionController {
     }
 
 
-
+    /**
+     * Este método maneja una solicitud GET a la ruta /habitaciones/editar/{id}.
+     * Busca un objeto Habitacion en la base de datos utilizando el id proporcionado como parámetro
+     * de la ruta. Si no se encuentra el objeto, se agrega un mensaje de error al modelo y se
+     * renderiza la plantilla "index".
+     * Si se encuentra el objeto, se agrega como un atributo del modelo y se renderiza la plantilla
+     * "editarHabitacion".
+     *
+     * @param model El objeto Spring Model utilizado para almacenar atributos del modelo
+     * @param id El id de la Habitacion a buscar y editar
+     * @return El nombre de la plantilla Thymeleaf a renderizar
+     */
     @GetMapping("/editar/{id}")
     public String editar(Model model, @PathVariable Integer id){
         Habitacion habitacion = habitacionService.findById(id);
@@ -124,11 +168,29 @@ public class HabitacionController {
 
 
 
+    /**
+     * Este método maneja una solicitud POST a la ruta /habitaciones/editar.
+     * Valida el objeto Habitacion proporcionado y comprueba si hay errores de validación.
+     * Si hay errores, se agrega un mensaje de error al modelo y se renderiza la plantilla
+     * "editarHabitacion" de nuevo.
+     * Si la validación es exitosa, se carga la imagen proporcionada y se guarda la Habitacion en la
+     * base de datos. Finalmente, se agrega un mensaje de éxito al modelo y se redirige a la ruta
+     * /habitaciones/listar/{idHotel}.
+     *
+     * @param habitacion El objeto Habitacion a validar y guardar
+     * @param result El objeto BindingResult que contiene los resultados de la validación
+     * @param model El objeto Spring Model utilizado para almacenar atributos del modelo
+     * @param status El objeto SessionStatus que se utiliza para marcar la sesión como completada
+     * @param idHotel El id del Hotel al que pertenece la Habitacion
+     * @param imagen La imagen a cargar para la Habitacion
+     * @param flash El objeto RedirectAttributes utilizado para agregar atributos flash al modelo
+     * @return El nombre de la plantilla Thymeleaf a renderizar o la ruta a la que se redirige
+     */
     @PostMapping("/editar")
     public String postEditar(@Valid Habitacion habitacion, BindingResult result, Model model, SessionStatus status,
                              @ModelAttribute("id") Integer idHotel, @RequestParam(value = "file") MultipartFile imagen,
-                             RedirectAttributes flash){
-        if(result.hasErrors()){
+                             RedirectAttributes flash) {
+        if(result.hasErrors()) {
             model.addAttribute("titulo", "Ha habido algún error");
             return "editarHabitacion";
         }
@@ -142,10 +204,23 @@ public class HabitacionController {
 
 
 
+
+    /**
+     * Este método maneja una solicitud GET a la ruta /habitaciones/borrar/{id}.
+     * Busca un objeto Habitacion en la base de datos utilizando el id proporcionado como parámetro
+     * de la ruta. Luego, borra la imagen asociada con la habitación y finalmente borra la habitación
+     * de la base de datos. Después de borrar la habitación, se redirige a la ruta
+     * /habitaciones/listar/{idHotel}.
+     *
+     * @param id El id de la Habitacion a buscar y borrar
+     * @param model El objeto Spring Model utilizado para almacenar atributos del modelo
+     * @param idHotel El id del Hotel al que pertenece la Habitacion
+     * @param flash El objeto RedirectAttributes utilizado para agregar atributos flash al modelo
+     * @return La ruta a la que se redirige después de borrar la Habitacion
+     */
     @GetMapping("/borrar/{id}")
     public String borrar(@PathVariable("id") Integer id, Model model, @ModelAttribute("id") Integer idHotel,
-                         RedirectAttributes flash){
-
+                         RedirectAttributes flash) {
 
         Habitacion habitacion = habitacionService.findById(id);
         habitacionService.borrarImagen(habitacion);
@@ -154,6 +229,7 @@ public class HabitacionController {
         model.addAttribute("success","Habitación borrada con éxito!");
         return "redirect:/habitaciones/listar/"+idHotel;
     }
+
 
     //Faker para generar habitaciones aleatorio.
    /* @GetMapping("/generar")
